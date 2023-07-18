70a3986e7f6784a3a68b5dfe848d83e5340a9ed0
Shajjad Shawon <109540869+Shajjad-142@users.noreply.github.com>
2023-07-19 04:31:30 +0600

    implementation *800

---
+++ cf_61_A_Ultra-Fast Mathematician.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string s1,s2;
+    int p=s1.size();
+    cin>>s1>>s2;
+    for(int i=0;i<s1.size();i++)
+    {
+
+        if(s1[i]==s2[i])
+        cout<<0;
+        else
+            cout<<1;
+    }
+
+    return 0;
+}
